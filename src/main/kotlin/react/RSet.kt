3d63507b5f3712ec0593ba37252bc402463ab3dc
Christoph Dietze <git@cdietze.de>
2017-07-01 23:48:40 +0200

    Port RMap and RSet from React

---
+++ src/main/kotlin/react/RSet.kt
@@ -0,0 +1,284 @@
+/*
+ * Copyright 2017 The React.kt Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package react
+
+/**
+ * Provides a reactive model of a set. Note that [.add] and other default mechanisms for
+ * updating the set *will not* trigger a notification if the updated element is equal to an
+ * element already in the set. Use [.addForce] to force a notification. Similarly, [ ][.remove] will only generate a notification if an element was actually removed, use [ ][.removeForce] to force a notification.
+ */
+class RSet<E>
+/**
+ * Creates a reactive set with the supplied underlying set implementation.
+ */
+(
+        /** Contains our underlying elements.  */
+        protected var _impl: MutableSet<E>) : RCollection<E>(), MutableSet<E> {
+    /** An interface for publishing set events to listeners.  */
+    abstract class Listener<E> : RListener() {
+        /** Notifies listener of an added element.  */
+        open fun onAdd(elem: E?) {
+            // noop
+        }
+
+        /** Notifies listener of a removed element.  */
+        open fun onRemove(elem: E?) {
+            // noop
+        }
+    }
+
+    /**
+     * Connects the supplied listener to this set, such that it will be notified on adds and
+     * removes.
+     * @return a connection instance which can be used to cancel the connection.
+     */
+    fun connect(listener: Listener<in E>): Connection {
+        return addConnection(listener)
+    }
+
+    /**
+     * Invokes `onAdd` for all existing elements, and then connects `listener`.
+     */
+    fun connectNotify(listener: Listener<in E>): Connection {
+        for (elem in this) listener.onAdd(elem)
+        return connect(listener)
+    }
+
+    /**
+     * Disconnects the supplied listener from this set if listen was called with it.
+     */
+    fun disconnect(listener: Listener<in E>) {
+        removeConnection(listener)
+    }
+
+    /**
+     * Adds the supplied element to the set, forcing a notification to the listeners regardless of
+     * whether the element was already in the set or not.
+     * @return true if the element was added, false if it was already in the set.
+     */
+    fun addForce(elem: E): Boolean {
+        checkMutate()
+        val added = _impl.add(elem)
+        emitAdd(elem)
+        return added
+    }
+
+    /**
+     * Removes the supplied element from the set, forcing a notification to the listeners
+     * regardless of whether the element was already in the set or not.
+     * @return true if the element was in the set and was removed, false if it was not.
+     */
+    fun removeForce(elem: E): Boolean {
+        checkMutate()
+        val removed = _impl.remove(elem)
+        emitRemove(elem)
+        return removed
+    }
+
+    /**
+     * Returns a value that models whether the specified element is contained in this map. The
+     * value will report a change when the specified element is added or removed. Note that [ ][.addForce] or [.removeForce] will cause this view to trigger and incorrectly report
+     * that the element was not or was previously contained in the set. Caveat user.
+     */
+    fun containsView(elem: E?): ValueView<Boolean> {
+        if (elem == null) throw NullPointerException("Must supply non-null 'elem'.")
+        return object : MappedValue<Boolean>() {
+            override fun get(): Boolean {
+                return contains(elem)
+            }
+
+            override fun connect(): Connection {
+                return this@RSet.connect(object : RSet.Listener<E>() {
+                    override fun onAdd(aelem: E?) {
+                        if (elem == aelem) notifyChange(true, false)
+                    }
+
+                    override fun onRemove(relem: E?) {
+                        if (elem == relem) notifyChange(false, true)
+                    }
+                })
+            }
+        }
+    }
+
+    // from interface Set<E>
+    override val size: Int get() {
+        return _impl.size
+    }
+
+    // from interface Set<E>
+    override fun isEmpty(): Boolean {
+        return _impl.isEmpty()
+    }
+
+    // from interface Set<E>
+    override operator fun contains(key: E): Boolean {
+        return _impl.contains(key)
+    }
+
+    // from interface Set<E>
+    override fun add(elem: E): Boolean {
+        checkMutate()
+        if (!_impl.add(elem)) return false
+        emitAdd(elem)
+        return true
+    }
+
+    // from interface Set<E>
+    override fun remove(rawElem: E): Boolean {
+        checkMutate()
+        if (!_impl.remove(rawElem)) return false
+        val elem = rawElem as E
+        emitRemove(elem)
+        return true
+    }
+
+    // from interface Set<E>
+    override fun containsAll(coll: Collection<E>): Boolean {
+        return _impl.containsAll(coll)
+    }
+
+    // from interface Set<E>
+    override fun addAll(coll: Collection<E>): Boolean {
+        var modified = false
+        for (elem in coll) {
+            modified = modified or add(elem)
+        }
+        return modified
+    }
+
+    // from interface Set<E>
+    override fun retainAll(coll: Collection<E>): Boolean {
+        var modified = false
+        val iter = iterator()
+        while (iter.hasNext()) {
+            if (!coll.contains(iter.next())) {
+                iter.remove()
+                modified = true
+            }
+        }
+        return modified
+    }
+
+    // from interface Set<E>
+    override fun removeAll(coll: Collection<E>): Boolean {
+        var modified = false
+        val iter = coll.iterator()
+        while (iter.hasNext()) {
+            modified = modified or remove(iter.next())
+        }
+        return modified
+    }
+
+    // from interface Set<E>
+    override fun clear() {
+        checkMutate()
+        // generate removed events for our elemens (do so on a copy of our set so that we can clear
+        // our underlying set before any of the published events are processed)
+        val elems = ArrayList(_impl)
+        _impl.clear()
+        for (elem in elems) emitRemove(elem)
+    }
+
+    // from interface Set<E>
+    override fun iterator(): MutableIterator<E> {
+        val iiter = _impl.iterator()
+        return object : MutableIterator<E> {
+            override fun hasNext(): Boolean {
+                return iiter.hasNext()
+            }
+
+            override fun next(): E {
+                _current = iiter.next()
+                return _current!!
+            }
+
+            override fun remove() {
+                checkMutate()
+                iiter.remove()
+                emitRemove(_current!!)
+            }
+
+            protected var _current: E? = null
+        }
+    }
+
+    override fun hashCode(): Int {
+        return _impl.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other === this || _impl == other
+    }
+
+    override fun toString(): String {
+        return "RSet" + _impl
+    }
+
+    override fun placeholderListener(): Listener<E> {
+        val p = NOOP as Listener<E>
+        return p
+    }
+
+    protected fun emitAdd(elem: E) {
+        notifyAdd(elem)
+    }
+
+    protected fun notifyAdd(elem: E) {
+        notify(ADD, elem, null, null)
+    }
+
+    protected fun emitRemove(elem: E) {
+        notifyRemove(elem)
+    }
+
+    protected fun notifyRemove(elem: E) {
+        notify(REMOVE, elem, null, null)
+    }
+
+    companion object {
+
+        /**
+         * Creates a reactive set backed by a @{link HashSet}.
+         */
+        fun <E> create(): RSet<E> {
+            return create(HashSet())
+        }
+
+        /**
+         * Creates a reactive set with the supplied underlying set implementation.
+         */
+        fun <E> create(impl: MutableSet<E>): RSet<E> {
+            return RSet(impl)
+        }
+
+        protected val NOOP: Listener<Any> = object : Listener<Any>() {
+
+        }
+
+        protected val ADD: Reactor.Notifier = object : Reactor.Notifier() {
+            override fun notify(lner: Any, elem: Any?, _1: Any?, _2: Any?) {
+                (lner as Listener<Any>).onAdd(elem)
+            }
+        }
+
+        protected val REMOVE: Reactor.Notifier = object : Reactor.Notifier() {
+            override fun notify(lner: Any, elem: Any?, _1: Any?, _2: Any?) {
+                (lner as Listener<Any>).onRemove(elem)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/react/RSetTest.kt
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2017 The React.kt Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package react
+
+import org.junit.Assert.*
+import org.junit.Test
+import java.util.*
+
+/**
+ * Tests aspects of the [RSet] class.
+ */
+class RSetTest {
+    class Counter : RSet.Listener<Any>() {
+        var notifies: Int = 0
+        override fun onAdd(elem: Any?) {
+            notifies++
+        }
+
+        override fun onRemove(elem: Any?) {
+            notifies++
+        }
+    }
+
+    @Test
+    fun testBasicNotify() {
+        val set = RSet.create(HashSet<Int>())
+        val counter = Counter()
+        set.connect(counter)
+
+        // add an element, ensure that we're notified
+        set.connect(requireAdd(42)).once()
+        set.add(42)
+        assertEquals(1, counter.notifies.toLong())
+
+        // add the same element, and ensure that we're not notified
+        set.add(42)
+        assertEquals(1, counter.notifies.toLong())
+
+        // force add the same element, ensure that we are notified
+        set.addForce(42)
+        assertEquals(2, counter.notifies.toLong())
+
+        // remove an element, ensure that we're notified
+        set.connect(requireRemove(42)).once()
+        set.remove(42)
+        assertEquals(3, counter.notifies.toLong())
+
+        // remove it again, ensure that we're not notified
+        set.remove(42)
+        assertEquals(3, counter.notifies.toLong())
+
+        // force remove it, ensure that we are notified
+        set.removeForce(42)
+        assertEquals(4, counter.notifies.toLong())
+
+        // remove a non-existent element, ensure that we're not notified
+        set.remove(25)
+        assertEquals(4, counter.notifies.toLong())
+    }
+
+    @Test
+    fun testAggregatesEtc() {
+        val set = RSet.create(HashSet<Int>())
+        val counter = Counter()
+        set.connect(counter)
+
+        // test adding multiple entries
+        set.addAll(Arrays.asList(1, 2, 3, 4))
+        assertEquals(4, counter.notifies.toLong())
+
+        // test removing by iterator
+        val iter = set.iterator()
+        iter.next()
+        iter.remove()
+        assertEquals(5, counter.notifies.toLong())
+        assertEquals(3, set.size.toLong())
+        val v1 = iter.next()
+        val v2 = iter.next()
+
+        // test notification on remove all
+        set.removeAll(Arrays.asList(v1, 5, 6))
+        assertEquals(6, counter.notifies.toLong())
+        assertEquals(2, set.size.toLong())
+
+        // test notification on retain all
+        set.retainAll(Arrays.asList(v2, 7, 8))
+        assertEquals(7, counter.notifies.toLong())
+        assertEquals(1, set.size.toLong())
+
+        // finally test notification on clear
+        set.clear()
+        assertEquals(8, counter.notifies.toLong())
+        assertEquals(0, set.size.toLong())
+    }
+
+    @Test
+    fun testContainsView() {
+        val set = RSet.create(HashSet<Int>())
+        set.add(1)
+
+        // create some contains key views and ensure their initial values are correct
+        val containsOne = set.containsView(1)
+        val containsTwo = set.containsView(2)
+        assertTrue(containsOne.get())
+        assertFalse(containsTwo.get())
+
+        // listen for notifications
+        val counter = SignalTest.Counter()
+        containsOne.connect(counter)
+        containsTwo.connect(counter)
+
+        // remove the element for one and ensure that we're notified
+        containsOne.connect(SignalTest.require(false)).once()
+        set.remove(1)
+        assertEquals(1, counter.notifies.toLong())
+
+        // make sure we're not repeat notified
+        set.remove(1)
+        assertEquals(1, counter.notifies.toLong())
+
+        // force a remove and make sure we are repeat notified
+        set.removeForce(1)
+        assertEquals(2, counter.notifies.toLong())
+
+        // add an element for two and ensure that we're notified
+        containsTwo.connect(SignalTest.require(true)).once()
+        set.add(2)
+        assertEquals(3, counter.notifies.toLong())
+
+        // make sure we're not repeat notified
+        set.add(2)
+        assertEquals(3, counter.notifies.toLong())
+
+        // force an add and make sure we are repeat notified
+        set.addForce(2)
+        assertEquals(4, counter.notifies.toLong())
+    }
+
+    @Test
+    fun testSizeView() {
+        val set: RSet<String> = RSet.create()
+        set.add("one")
+        assertEquals(1, set.sizeView().get().toLong())
+        set.remove("one")
+        assertEquals(0, set.sizeView().get().toLong())
+
+        val counter = SignalTest.Counter()
+        set.sizeView().connect(counter)
+        set.add("two")
+        assertEquals(1, counter.notifies.toLong())
+        set.add("three")
+        assertEquals(2, counter.notifies.toLong())
+        set.remove("two")
+        assertEquals(3, counter.notifies.toLong())
+        // make sure noops don't trigger size view
+        set.remove("two")
+        assertEquals(3, counter.notifies.toLong())
+        set.add("three")
+        assertEquals(3, counter.notifies.toLong())
+    }
+
+    companion object {
+
+        protected fun <T> requireAdd(reqElem: T): RSet.Listener<T> {
+            return object : RSet.Listener<T>() {
+                override fun onAdd(elem: T?) {
+                    assertEquals(reqElem, elem)
+                }
+            }
+        }
+
+        protected fun <T> requireRemove(reqElem: T): RSet.Listener<T> {
+            return object : RSet.Listener<T>() {
+                override fun onRemove(elem: T?) {
+                    assertEquals(reqElem, elem)
+                }
+            }
+        }
+    }
+}
